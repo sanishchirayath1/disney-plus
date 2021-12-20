6b2ce7132c50eb314d6dd670cd10d774711bca8d
sanishchirayath1 <sanishchirayath@gmail.com>
2021-12-20 20:06:43 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Details from './components/Details';
+import Login from './components/Login';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/details" element={<Details />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Login.css"
+
+function Login() {
+    return (
+        <div className='login-container'>
+            <div className='login'>
+                <img src="/images/cta-logo-one.svg"/>
+                <a className='btn login-btn'>GET ALL HERE</a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quia vero minima quae tempora eveniet dolorum, consectetur praesentium! Sit, magnam!</p>
+                <img src="/images/cta-logo-two.png"/>
+            </div>
+        </div>
+    )
+}
+
+export default Login
