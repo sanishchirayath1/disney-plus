7f358b9c7a03cf3059c4f8f8bca0f4bd3b36e279
sanishchirayath1 <sanishchirayath@gmail.com>
2021-12-21 18:56:09 +0530

    viewers  styled update

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -28,7 +28,6 @@ function Viewers() {
 export default Viewers
 
 const Wrap = styled.div`
-  padding-top: 56.25%;
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
@@ -37,25 +36,23 @@ const Wrap = styled.div`
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
+  
   img {
-    inset: 0px;
-    display: block;
     height: 100%;
+    width: 100%;
     object-fit: cover;
     opacity: 1;
-    position: absolute;
+    z-index: 2;
     transition: opacity 500ms ease-in-out 0s;
-    width: 100%;
-    z-index: 1;
-    top: 0;
   }
   video {
     width: 100%;
     height: 100%;
     position: absolute;
-    top: 0px;
+    top: 0;
+    left:0;
     opacity: 0;
-    z-index: 0;
+    z-index: -1;
   }
   &:hover {
     box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
